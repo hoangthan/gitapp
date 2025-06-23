hoangthan/note-app

Create note app

app/src/main/java/com/note/assessment/main/MainActivity.kt
@@ -1,0 +1,45 @@
+package com.note.assessment.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.note.features.core.ui.ComposeAppTheme
+import com.note.features.note.list.NoteListScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(WindowInsets.systemBars.asPaddingValues())
+                    ) {
+                        NoteListScreen()
+                    }
+                }
+            }
+        }
+    }
+}

app/src/main/java/com/note/assessment/main/MainApplication.kt
@@ -1,0 +1,7 @@
+package com.note.assessment.main
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()

features/core/src/main/java/com/note/features/core/ui/Colors.kt
@@ -1,0 +1,25 @@
+package com.note.features.core.ui
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+    val PrimaryLight = Color(0xFF1565C0)
+    val OnPrimaryLight = Color(0xFFFFFFFF)
+
+    val PrimaryDark = Color(0xFF90CAF9)
+    val OnPrimaryDark = Color(0xFF000000)
+
+    val BackgroundLight = Color(0xFFFFFFFF)
+    val OnBackgroundLight = Color(0xFF000000)
+
+    val BackgroundDark = Color(0xFF121212)
+    val OnBackgroundDark = Color(0xFFFFFFFF)
+
+    val SurfaceLight = Color(0xFFFFFFFF)
+    val OnSurfaceLight = Color(0xFF1C1B1F)
+    val SurfaceVariantLight = Color(0xFFE0E0E0)
+
+    val SurfaceDark = Color(0xFF1E1E1E)
+    val OnSurfaceDark = Color(0xFFECECEC)
+    val SurfaceVariantDark = Color(0xFF2C2C2C)
+}

features/core/src/main/java/com/note/features/core/ui/Dimens.kt
@@ -1,0 +1,27 @@
+package com.note.features.core.ui
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val smallPadding = 8.dp
+    val mediumPadding = 16.dp
+    val largePadding = 24.dp
+    
+    val smallSpacing = 4.dp
+    val mediumSpacing = 8.dp
+    val largeSpacing = 16.dp
+    
+    val buttonHeight = 44.dp
+    val navigationBarHeight = 44.dp
+    val tabBarHeight = 49.dp
+    
+    val smallCornerRadius = 4.dp
+    val mediumCornerRadius = 8.dp
+    val largeCornerRadius = 12.dp
+    
+    val smallIconSize = 16.dp
+    val mediumIconSize = 24.dp
+    val largeIconSize = 32.dp
+
+    val elevation = 4.dp
+}

features/core/src/main/java/com/note/features/core/ui/Theme.kt
@@ -1,0 +1,41 @@
+package com.note.features.core.ui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColors = lightColorScheme(
+    primary = Colors.PrimaryLight,
+    onPrimary = Colors.OnPrimaryLight,
+    background = Colors.BackgroundLight,
+    onBackground = Colors.OnBackgroundLight,
+    surface = Colors.SurfaceLight,
+    onSurface = Colors.OnSurfaceLight,
+    surfaceVariant = Colors.SurfaceVariantLight
+)
+
+private val DarkColors = darkColorScheme(
+    primary = Colors.PrimaryDark,
+    onPrimary = Colors.OnPrimaryDark,
+    background = Colors.BackgroundDark,
+    onBackground = Colors.OnBackgroundDark,
+    surface = Colors.SurfaceDark,
+    onSurface = Colors.OnSurfaceDark,
+    surfaceVariant = Colors.SurfaceVariantDark
+)
+
+@Composable
+fun ComposeAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColors else LightColors
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        content = content
+    )
+}

features/core/src/main/java/com/note/features/core/ui/Typography.kt
@@ -1,0 +1,99 @@
+package com.note.features.core.ui
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 34.sp,
+        lineHeight = 41.sp,
+        letterSpacing = 0.37.sp
+    ),
+    displayMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 34.sp,
+        letterSpacing = 0.sp
+    ),
+    displaySmall = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 29.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 25.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = 23.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 21.sp,
+        letterSpacing = 0.sp
+    ),
+    titleLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 17.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontWeight = FontWeight.W500,
+        fontSize = 17.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 17.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    bodySmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.sp
+    ),
+    labelLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.sp
+    ),
+    labelMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp
+    )
+)

libraries/core/src/main/java/com/note/data/core/coroutine/DispatcherProvider.kt
@@ -1,0 +1,33 @@
+package com.note.data.core.coroutine
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+import javax.inject.Singleton
+
+interface DispatcherProvider {
+    val io: CoroutineDispatcher
+    val main: CoroutineDispatcher
+    val default: CoroutineDispatcher
+    val unConfined: CoroutineDispatcher
+}
+
+internal class AppDispatcherProvider @Inject constructor() : DispatcherProvider {
+    override val io: CoroutineDispatcher = Dispatchers.IO
+    override val main: CoroutineDispatcher = Dispatchers.Main
+    override val default: CoroutineDispatcher = Dispatchers.Default
+    override val unConfined: CoroutineDispatcher = Dispatchers.Unconfined
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class DispatcherProviderModule {
+
+    @Binds
+    @Singleton
+    abstract fun provideAppDispatcher(impl: AppDispatcherProvider): DispatcherProvider
+}

libraries/core/src/main/java/com/note/data/core/utils/Extension.kt
@@ -1,0 +1,10 @@
+package com.note.data.core.utils
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+fun Long.toFormattedDate(): String {
+    val dateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.getDefault())
+    return dateFormat.format(Date(this))
+}

hoangthan/gitapp

app/src/main/java/com/note/assessment/main/MainActivity.kt
@@ -1,0 +1,44 @@
+package com.git.assessment.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.git.features.core.ui.ComposeAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(WindowInsets.systemBars.asPaddingValues())
+                    ) {
+                        //NoteListScreen()
+                    }
+                }
+            }
+        }
+    }
+}

app/src/main/java/com/note/assessment/main/MainApplication.kt
@@ -1,0 +1,7 @@
+package com.git.assessment.main
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()

features/core/src/main/java/com/note/features/core/ui/Colors.kt
@@ -1,0 +1,25 @@
+package com.git.features.core.ui
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+    val PrimaryLight = Color(0xFF1565C0)
+    val OnPrimaryLight = Color(0xFFFFFFFF)
+
+    val PrimaryDark = Color(0xFF90CAF9)
+    val OnPrimaryDark = Color(0xFF000000)
+
+    val BackgroundLight = Color(0xFFFFFFFF)
+    val OnBackgroundLight = Color(0xFF000000)
+
+    val BackgroundDark = Color(0xFF121212)
+    val OnBackgroundDark = Color(0xFFFFFFFF)
+
+    val SurfaceLight = Color(0xFFFFFFFF)
+    val OnSurfaceLight = Color(0xFF1C1B1F)
+    val SurfaceVariantLight = Color(0xFFE0E0E0)
+
+    val SurfaceDark = Color(0xFF1E1E1E)
+    val OnSurfaceDark = Color(0xFFECECEC)
+    val SurfaceVariantDark = Color(0xFF2C2C2C)
+}

features/core/src/main/java/com/note/features/core/ui/Dimens.kt
@@ -1,0 +1,27 @@
+package com.git.features.core.ui
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val smallPadding = 8.dp
+    val mediumPadding = 16.dp
+    val largePadding = 24.dp
+    
+    val smallSpacing = 4.dp
+    val mediumSpacing = 8.dp
+    val largeSpacing = 16.dp
+    
+    val buttonHeight = 44.dp
+    val navigationBarHeight = 44.dp
+    val tabBarHeight = 49.dp
+    
+    val smallCornerRadius = 4.dp
+    val mediumCornerRadius = 8.dp
+    val largeCornerRadius = 12.dp
+    
+    val smallIconSize = 16.dp
+    val mediumIconSize = 24.dp
+    val largeIconSize = 32.dp
+
+    val elevation = 4.dp
+}

features/core/src/main/java/com/note/features/core/ui/Theme.kt
@@ -1,0 +1,41 @@
+package com.git.features.core.ui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColors = lightColorScheme(
+    primary = Colors.PrimaryLight,
+    onPrimary = Colors.OnPrimaryLight,
+    background = Colors.BackgroundLight,
+    onBackground = Colors.OnBackgroundLight,
+    surface = Colors.SurfaceLight,
+    onSurface = Colors.OnSurfaceLight,
+    surfaceVariant = Colors.SurfaceVariantLight
+)
+
+private val DarkColors = darkColorScheme(
+    primary = Colors.PrimaryDark,
+    onPrimary = Colors.OnPrimaryDark,
+    background = Colors.BackgroundDark,
+    onBackground = Colors.OnBackgroundDark,
+    surface = Colors.SurfaceDark,
+    onSurface = Colors.OnSurfaceDark,
+    surfaceVariant = Colors.SurfaceVariantDark
+)
+
+@Composable
+fun ComposeAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColors else LightColors
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        content = content
+    )
+}

features/core/src/main/java/com/note/features/core/ui/Typography.kt
@@ -1,0 +1,99 @@
+package com.git.features.core.ui
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 34.sp,
+        lineHeight = 41.sp,
+        letterSpacing = 0.37.sp
+    ),
+    displayMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 34.sp,
+        letterSpacing = 0.sp
+    ),
+    displaySmall = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 29.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 25.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = 23.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 21.sp,
+        letterSpacing = 0.sp
+    ),
+    titleLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 17.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontWeight = FontWeight.W500,
+        fontSize = 17.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 17.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    bodySmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.sp
+    ),
+    labelLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.sp
+    ),
+    labelMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp
+    )
+)

libraries/core/src/main/java/com/note/data/core/coroutine/DispatcherProvider.kt
@@ -1,0 +1,33 @@
+package com.git.data.core.coroutine
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+import javax.inject.Singleton
+
+interface DispatcherProvider {
+    val io: CoroutineDispatcher
+    val main: CoroutineDispatcher
+    val default: CoroutineDispatcher
+    val unConfined: CoroutineDispatcher
+}
+
+internal class AppDispatcherProvider @Inject constructor() : DispatcherProvider {
+    override val io: CoroutineDispatcher = Dispatchers.IO
+    override val main: CoroutineDispatcher = Dispatchers.Main
+    override val default: CoroutineDispatcher = Dispatchers.Default
+    override val unConfined: CoroutineDispatcher = Dispatchers.Unconfined
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class DispatcherProviderModule {
+
+    @Binds
+    @Singleton
+    abstract fun provideAppDispatcher(impl: AppDispatcherProvider): DispatcherProvider
+}

libraries/core/src/main/java/com/note/data/core/utils/Extension.kt
@@ -1,0 +1,10 @@
+package com.git.data.core.utils
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+fun Long.toFormattedDate(): String {
+    val dateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.getDefault())
+    return dateFormat.format(Date(this))
+}